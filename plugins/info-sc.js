9d16e616646c1fcc628e2cbd2bc8673dda3d9fe5
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-16 01:01:23 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCNBiajdnzMs8KJE19bf-OXA*\nSubscribe Untuk Mengetahui Update Selanjutnya:https://github.com/ShionMDv Follow githubku dan kasih star cuy `
+let str = `*https://youtube.com/channel/UCNBiajdnzMs8KJE19bf-OXA*\nSubscribe Untuk sc gw privasi cuy `
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
